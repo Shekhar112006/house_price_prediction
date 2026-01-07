066a06522321cbf84e05371e1a9d4d2196d3d683
Shekhar Gade <shekhargade398@gmail.com>
2026-01-07 22:23:05 +0530

    changes done

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,10 +1,10 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def load_data(path):
-    data=pd.read_csv(path)
+def load_and_split_data(path):
+    data = pd.read_csv(path)
 
-    x=data[['area','bedrooms','bathrooms']]
-    y=data['price']
+    X = data[['area', 'bedrooms', 'bathrooms']]
+    y = data['price']
 
-    return train_test_split(x,y,test_size=0.2,random_state=42)
+    return train_test_split(X, y, test_size=0.2, random_state=42)
